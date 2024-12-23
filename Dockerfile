e36010f5abc1944f22641e7d804b838c85a597f0
Ibrahim Awad <ibrahim.awad@btech.com>
2024-12-23 07:55:44 +0200

    feat(infra): add dedicated server docker file

---
+++ Dockerfile
@@ -0,0 +1,37 @@
+#
+# conductor:server - Netflix conductor server
+#
+# ===========================================================================================================
+# 0. Builder stage
+# ===========================================================================================================
+
+FROM alpine:3.18 AS builder
+LABEL maintainer="Ibrahim Awad <ibrahim.a.hamid@gmail.com>"
+
+COPY . /app
+RUN apk add openjdk17
+
+WORKDIR /app
+RUN ./gradlew clean build -x test && echo "Done building Server"
+
+# ===========================================================================================================
+# 1. Runtime stage
+# ===========================================================================================================
+
+FROM alpine:3.18 AS runner
+LABEL maintainer="Ibrahim Awad <ibrahim.a.hamid@gmail.com>"
+
+RUN apk add --no-cache coreutils curl nginx openjdk17 && rm -rf /var/cache/apk/* \
+	&& mkdir -p /app/config /app/logs /app/libs /app/info
+
+COPY ./docker/config/startup.sh /app/startup.sh
+COPY ./docker/config/config.properties /app/config/config.properties
+
+COPY --from=builder /app/server/build/libs/orkes-conductor-server-boot.jar /app/libs/server.jar
+
+HEALTHCHECK --interval=60s --timeout=30s --retries=10 CMD ["curl", "-I", "-XGET", "http://localhost:8080/health"]
+
+EXPOSE 8080
+
+CMD ["/app/startup.sh"]
+ENTRYPOINT ["/bin/sh"]
--- server/src/main/java/io/orkes/conductor/config/SecurityDisabledConfig.java
+++ server/src/main/java/io/orkes/conductor/config/SecurityDisabledConfig.java
@@ -12,9 +12,13 @@
  */
 package io.orkes.conductor.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 @Configuration
 public class SecurityDisabledConfig extends WebSecurityConfigurerAdapter {
@@ -24,5 +28,19 @@ public class SecurityDisabledConfig extends WebSecurityConfigurerAdapter {
     }
 
     @Override
-    protected void configure(HttpSecurity http) throws Exception {}
+    protected void configure(HttpSecurity http) throws Exception {
+        http.cors().and().csrf().disable();
+    }
+
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(false);
+        config.addAllowedOrigin("*");
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
+    }
 }
